865138f7d5772456d828d308c2c82ef7b7acb2f4
natorion <michael-git@hablich.at>
2014-05-23 13:16:48 +0200

    Extracted navbar & Color changes

---
+++ includes/navbar.js
@@ -0,0 +1,33 @@
+var navbarDivision = document.getElementById("navbar");
+
+navbarDivision.innerHTML = '<div class="navbar navbar-inverse" role="navigation">\
+    <div class="container">\
+    <div class="navbar-header">\
+        <a class="navbar-brand" href="index.html">Michael Hablich\'s Filing Cabinet</a>\
+    </div>\
+    <div class="collapse navbar-collapse">\
+    <ul class="nav navbar-nav">\
+        <li><a href="index.html">Cabinet</a></li>\
+        <li><a href="bio.html">Bio</a></li>\
+    </ul>\
+    </div>\
+    </div>\
+    </div>';
+
+/* old:
+
+    <div class="navbar navbar-inverse" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.html">Michael Hablich's Filing Cabinet</a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.html">Cabinet</a></li>
+                    <li><a href="bio.html">Bio</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+*/
