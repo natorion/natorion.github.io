c7f1a5d094fbf8ef8f7a639e5bf3462057d98c3c
natorion <michael-git@hablich.at>
2014-05-23 15:27:49 +0200

    Fixed mobile navigation

--- includes/navbar.js
+++ includes/navbar.js
@@ -3,6 +3,12 @@ var navbarDivision = document.getElementById("navbar");
 navbarDivision.innerHTML = '<div class="navbar navbar-inverse" role="navigation">\
     <div class="container">\
     <div class="navbar-header">\
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
+            <span class="sr-only">Toggle navigation</span>\
+            <span class="icon-bar"></span>\
+            <span class="icon-bar"></span>\
+            <span class="icon-bar"></span>\
+        </button>\
         <a class="navbar-brand" href="index.html">Michael Hablich\'s Filing Cabinet</a>\
     </div>\
     <div class="collapse navbar-collapse">\
