4a52e3c912c6e117299d8fa2972368e9d240babf
natorion <michael-git@hablich.at>
2014-05-24 12:57:34 +0200

    Fixed footer Usable version of bio

--- includes/footer.js
+++ includes/footer.js
@@ -1,1 +1,2 @@
-document.writeln('<div class="footer container">© Michael Hablich 2014; Built with <a href="http://getbootstrap.com/">Bootstrap</a></div>');
+var footerSpan = document.getElementById("footer");
+footerSpan.innerHTML = '<div class="footer container">© Michael Hablich 2014; Built with <a href="http://getbootstrap.com/">Bootstrap</a></div>';
