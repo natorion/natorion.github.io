64ff3bb4a6bd3048f0ae80d418dbf09a2817f3f6
Michael Hablich <michael.hablich@gmail.com>
2023-03-22 18:39:29 +0100

    Add DT script to try it out

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,7 @@
 export default {
     title: 'Michael Hablich\'s Filing Cabinet',
-    description: 'Just playing around.'
+    description: 'Just playing around.',
+    head: [
+      ['script', {src: '/dt_script.js'}]
+    ]
   }
