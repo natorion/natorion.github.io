bbee1ad7b4cea3e2eafd8bd6d0719c0646d4ecf4
Michael Hablich <michael.hablich@gmail.com>
2024-07-04 18:34:08 +0200

    Add tag manager GA 4

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,19 @@
 export default {
     title: 'Michael Hablich\'s Filing Cabinet',
-    description: 'Just playing around.'
+    description: 'Content about software product development.',
+
+    head: [
+      [
+        'script',
+        { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=448982390' }
+      ],
+      [
+        'script',
+        {},
+        `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '448982390');`
+      ]
+    ]
   }
